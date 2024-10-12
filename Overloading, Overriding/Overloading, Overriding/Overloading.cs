46b9a4c8a9ec422263311e44e03f065212ac1162
SaDongHuck <tkehdgur0001@gmail.com>
2024-10-13 01:09:30 +0900

    오버로딩, 오버라이딩
    오버로딩, 오버라이딩 정리

---
+++ Overloading, Overriding/Overloading, Overriding/Overloading.cs
@@ -0,0 +1,43 @@
+﻿namespace Overloading__Overriding
+{
+    internal class Overloading
+    {
+        public class Test
+        {
+            public void print()
+            {
+                Console.WriteLine("hello");
+            }
+
+            public void print(int num)
+            {
+                Console.WriteLine(num);
+            }//같은 이름 함수(메개변수 다르게)
+             //오버로딩
+            public void print(string str)
+            {
+                Console.WriteLine(str);
+            }
+            public void print(string str, int num)
+            {
+                for(int i = 0; i < num; i++)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            //다향성
+            //하나의 코드가 여러 가지 형태로 실행될 수 있도록 하는 것
+            //오버로딩, 오버라이딩
+            //오버로딩: 같은 이름의 메서드를 여러개 정의
+            //오버라이딩: 부모 클래스에서 정의된 메서드를 자식 클래스에서 재정의
+            Test test = new Test();
+            test.print();
+            test.print(10);
+            test.print("HI");
+            test.print("c#은 어렵지 않다!!", 3);
+        }
+    }
+}
---
+++ Overloading, Overriding/Overriding/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Overriding
+{
+    internal class Program
+    {
+        public class Parent
+        {
+            public void print()
+            {
+                Console.WriteLine("부모 클래스 메서드 입니다");
+            }
+        }
+
+        public class Child : Parent
+        {
+            public void print()
+            {
+                Console.WriteLine("자식 클래스 메서드 입니다");
+            }
+
+        }// 부모 클래스에서 상속 받아 재정의(오버라이딩)
+
+        static void Main(string[] args)
+        {
+            //다향성
+            //하나의 코드가 여러 가지 형태로 실행될 수 있도록 하는 것
+            //오버로딩, 오버라이딩
+            //오버로딩: 같은 이름의 메서드를 여러개 정의
+            //오버라이딩: 부모 클래스에서 정의된 메서드를 자식 클래스에서 재정의
+            Parent parent = new Parent();
+            Child child = new Child();
+
+            parent.print();
+            child.print();
+        }
+    }
+}
