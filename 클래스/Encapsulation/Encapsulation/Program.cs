b705bcfdca4401b502840127b22b785d0a5d7fab
SaDongHuck <tkehdgur0001@gmail.com>
2024-10-14 08:45:50 +0900

    클래스
    c# 클래스 정리!!

---
+++ 클래스/Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        public class person
+        {
+            //데이터 필드
+            private int age; //나이 필드 만듦
+
+            public void setAge(int age)
+            { this.age = age; } //this -> 외부에서 클랙스에 접근했을때 자기 객체(클래스가 가진 age)
+
+            public int getAge()
+            { return age; } //반환   
+
+        }
+        static void Main(string[] args)
+        {
+            //캡슐화
+            //데이터 메서드를 묶음
+            //class
+            //데이터(필드) 외부에서 접근할 수 없게 만듦
+            //메서드를 외부에 접근할 수 있게 만듦
+            //알약
+            //private 접근제한자를 필드에 적용
+            person p = new person();
+            p.setAge(10);
+            Console.WriteLine(p.getAge());
+        }
+    }
+}
---
+++ 클래스/클래스/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace 클래스
+{
+    internal class Program
+    {
+        public class person
+        {
+            //데이터들 필드
+            public int height;
+            public int weight;
+
+            //메서드 (함수)
+            
+            public void printHeight()
+            {
+                Console.WriteLine("키 : " +  height);
+            }
+
+            public void printWeight()
+            {
+                Console.WriteLine("몸무게 : " + weight);
+            }
+        }//클래스는 설계도 이다
+        static void Main(string[] args)
+        {
+            //클래스
+            //클래스 -> 데이터 와 메서드를 하나로 묶음
+
+            //사람 = 키, 몸무게, 머리카락색깔, 발 사이즈
+            //메서드 = 키를 저장, 키를 변경, 머리카락색깔 변경
+
+            //클래스 = 붕어빵 틀
+            //객체 = 붕어빵
+
+            person p = new person();
+
+            p.height = 180;
+            p.weight = 70;
+            p.printHeight();
+            p.printWeight();
+            
+        }
+    }
+}
